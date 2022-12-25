3157d1c8b1f79b73ef4aa863e640e5e1beec7241
Mounesh <mouneshknaik@gmail.com>
2022-12-25 13:04:21 +0530

    sdf

--- stock_api/service.js
+++ stock_api/service.js
@@ -1,13 +1,7 @@
 var request = require('request');
 var mysql = require('mysql');
-const pool = mysql.createPool({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "",
-  connectionLimit: 2000, 
-  database: "stock"
-});
+const db=require("./db");
+const pool = mysql.createPool(db.db);
 
 
 
